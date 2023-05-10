fb768c7811a57ca5b950eb97acee2771d88a5128
p19605 <leonie.roth@css.ch>
2023-05-10 12:31:43 +0200

    add horizontal grid and pictures

---
+++ src/app/components/grid-horizontal/grid-horizontal.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GridHorizontalComponent } from './grid-horizontal.component';
+
+describe('GridHorizontalComponent', () => {
+  let component: GridHorizontalComponent;
+  let fixture: ComponentFixture<GridHorizontalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GridHorizontalComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GridHorizontalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
