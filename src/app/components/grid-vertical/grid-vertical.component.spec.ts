3802962722fe16a43cf5e0c2c2e0b46d174a058f
DawidKapka <kapkadawid6@gmail.com>
2023-05-10 11:03:29 +0200

    create header and vertical grid

---
+++ src/app/components/grid-vertical/grid-vertical.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GridVerticalComponent } from './grid-vertical.component';
+
+describe('GridVerticalComponent', () => {
+  let component: GridVerticalComponent;
+  let fixture: ComponentFixture<GridVerticalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GridVerticalComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GridVerticalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
