882494db6629e85a26803a697e96702a828e104e
DawidKapka <kapkadawid6@gmail.com>
2023-05-16 19:51:01 +0200

    finish animations page

--- src/app/animations/animations.component.ts
+++ src/app/animations/animations.component.ts
@@ -7,13 +7,24 @@ import {Component, HostListener, OnInit} from '@angular/core';
 })
 export class AnimationsComponent implements OnInit {
 
+  private canvas: any;
+  private ctx: any;
+  private x = 50;
+  private y = 50;
+  private back: boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+    const scrollAnimationDiv: HTMLElement = document.querySelector('.sc-animation')!;
+    this.handleScroll();
+    this.canvas = document.querySelector('canvas')
+    this.ctx = this.canvas.getContext('2d')
+    this.draw();
   }
 
   @HostListener('window:scroll', ['$event'])
-  handleScroll(event: any) {
+  handleScroll() {
     if (this.isScrollAnimationVisible()) {
       const scrollAnimationDiv = document.querySelector('.sc-animation');
       const fullScroll = scrollAnimationDiv!.clientHeight - 200;
@@ -22,7 +33,10 @@ export class AnimationsComponent implements OnInit {
       if (scrollPercentage <= 100 && scrollPercentage >= 0) {
         const animatedDiv: HTMLElement = document.querySelector('.animated-div')!;
         const animatedHeader: HTMLElement = document.querySelector('.animated-h1')!;
-          if  (scrollPercentage > 50) {
+          if (scrollPercentage > 90) {
+            animatedDiv.style.height = `${100 - ((scrollPercentage - 90) * 10)}%`;
+          }
+          else if  (scrollPercentage > 50) {
             animatedDiv.style.marginLeft = `${scrollPercentage - 50}%`
             animatedHeader.style.opacity = `${(scrollPercentage - 50) / 100}`
           } else {
@@ -38,5 +52,26 @@ export class AnimationsComponent implements OnInit {
     const scrollAnimationDiv = document.querySelector('.sc-animation')
     const top = scrollAnimationDiv?.getBoundingClientRect().top || 0;
     return window.innerHeight - 200 > top;
+  }
+
+  private clearScreen() {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+  }
+
+  private drawSquare(x: number, y: number) {
+    this.ctx.fillRect(x, y, 100, 100);
+  }
+
+  private draw() {
+    this.clearScreen();
+    this.drawSquare(this.x, this.y);
+    if (this.x > 200) {
+      this.back = true
+    }
+    if (this.x === 0) {
+      this.back = false
+    }
+    this.x = this.back ? this.x - 5 : this.x + 5;
+    setTimeout(() => this.draw(), 50);
   }
 }
